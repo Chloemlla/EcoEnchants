84b537f2fad283843442dd615fa33add068347bd
Fanyi Meng <happyclovo@gmail.com>
2026-06-20 11:36:11 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,12 +50,12 @@ base {
 
 dependencies {
     implementation(project(":eco-core:core-plugin"))
-    implementation(project(":eco-core:core-nms:v1_21_8", configuration = "reobf"))
-    implementation(project(":eco-core:core-nms:v1_21_10", configuration = "reobf"))
-    implementation(project(":eco-core:core-nms:v1_21_11", configuration = "reobf"))
-    implementation(project(":eco-core:core-nms:v26_1_1", configuration = "shadow"))
-    implementation(project(":eco-core:core-nms:v26_1_2", configuration = "shadow"))
-    implementation(project(":eco-core:core-nms:v26_2", configuration = "shadow"))
+    implementation(project(":eco-core:core-nms:v1_21_8", "reobf"))
+    implementation(project(":eco-core:core-nms:v1_21_10", "reobf"))
+    implementation(project(":eco-core:core-nms:v1_21_11", "reobf"))
+    implementation(project(":eco-core:core-nms:v26_1_1", "shadow"))
+    implementation(project(":eco-core:core-nms:v26_1_2", "shadow"))
+    implementation(project(":eco-core:core-nms:v26_2", "shadow"))
 
     embeddedLibreforge("com.willfp:libreforge:${libreforgeVersion!!}:shadow@jar")
     decompiler("org.vineflower:vineflower:$vineflowerVersion")
